f766293f0262c00acf2c75ab21ca5f5e9dd6c408
bagel <ponomarenko.pavel.web@gmail.com>
2026-01-31 14:18:56 +0200

    dual styles config (scss or less)

--- webpack/loaders/styles.js
+++ webpack/loaders/styles.js
@@ -1,15 +1,32 @@
-module.exports = () => ({
-  test: /\.(css|sass|scss)$/,
-  use: [
-    'css-loader',
-    {
-      loader: 'postcss-loader',
-      options: {
-        postcssOptions: {
-          plugins: ['autoprefixer'],
-        },
+module.exports = (env = {}) => {
+  const useLess = env.styles === 'less';
+
+  const preProcessorLoader = useLess ? {
+    loader: 'less-loader',
+    options: {
+      lessOptions: {
+        javascriptEnabled: true,
       },
     },
-    'sass-loader',
-  ],
-});
+  } : {
+    loader: 'sass-loader',
+  };
+
+  const test = useLess ? /\.(css|less)$/ : /\.(css|sass|scss)$/;
+
+  return {
+    test,
+    use: [
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: ['autoprefixer'],
+          },
+        },
+      },
+      preProcessorLoader,
+    ],
+  };
+};
